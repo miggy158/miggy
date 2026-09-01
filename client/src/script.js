49c014883529be52addfe12b2ed6a5ab1a400408
miggy158 <ericmigueljadraque95@gmail.com>
2026-09-01 20:59:53 +0800

    UPDATED

--- client/src/script.js
+++ client/src/script.js
@@ -1,30 +1,15 @@
-
 const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener('change', function (e) {
 	if (e.target.checked) {
 		document.body.classList.add('dark-mode');
+		localStorage.setItem('theme', 'dark');
 	} else {
 		document.body.classList.remove('dark-mode');
+		localStorage.setItem('theme', 'light');
 	}
 });
 
-function toggleSkills() {
-	const isFullscreen = document.body.classList.toggle("skills-fullscreen");
-	const hiddenSkills = document.querySelectorAll(".skills-hidden");
 
-	hiddenSkills.forEach((el) => el.classList.toggle("active", isFullscreen));
 
-	document.getElementById("viewAllLabel").textContent = isFullscreen
-		? "View Less"
-		: "View All";
-}
 
-function exitSkillsFullscreen() {
-	document.body.classList.remove("skills-fullscreen");
-
-	const hiddenSkills = document.querySelectorAll(".skills-hidden");
-
-	hiddenSkills.forEach((el) => el.classList.remove("active"));
-	document.getElementById("viewAllLabel").textContent = "View All";
-}
