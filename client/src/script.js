66b06725b68c8515ec0a4c1872bf19d0d0e8f76b
miggy158 <ericmigueljadraque95@gmail.com>
2026-09-03 10:29:58 +0800

    UPDATED

--- client/src/script.js
+++ client/src/script.js
@@ -1,15 +1,18 @@
 const themeToggle = document.getElementById('theme-toggle');
 
-themeToggle.addEventListener('change', function (e) {
-	if (e.target.checked) {
-		document.body.classList.add('dark-mode');
-		localStorage.setItem('theme', 'dark');
-	} else {
-		document.body.classList.remove('dark-mode');
-		localStorage.setItem('theme', 'light');
-	}
-});
+if (localStorage.getItem('theme') === 'dark') {
+	document.body.classList.add('dark-mode');
+	if (themeToggle) themeToggle.checked = true;
+}
 
-
-
-
+if (themeToggle) {
+	themeToggle.addEventListener('change', function (e) {
+		if (e.target.checked) {
+			document.body.classList.add('dark-mode');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			document.body.classList.remove('dark-mode');
+			localStorage.setItem('theme', 'light');
+		}
+	});
+}
